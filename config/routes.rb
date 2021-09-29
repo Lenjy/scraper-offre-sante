82918f015cd5cc55550f182b9e4f4598859e259d
Benjamin Eycken <be.eycken@gmail.com>
2021-09-29 22:43:21 +0200

    view progression

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :offers
+  root to: 'pages#home'
+  resources :offers, only: [:index, :show, :edit, :update, :destroy]
 end
